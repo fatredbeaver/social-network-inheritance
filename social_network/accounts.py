f3c48609a5f6cbc13df5250343cb646ab3477478
fatredbeaver <tedmundho@gmail.com>
2019-11-22 16:00:05 +0800

    Add files via upload

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,24 @@
 
 class User(object):
+    posts = []
+    following = []
+    timeline = []
+
     def __init__(self, first_name, last_name, email):
-        pass
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
 
     def add_post(self, post):
-        pass
+        post.set_user(self)
+        self.posts.append(post.text)
+        if self in user.following:
+            user.following.insert(0, post.text)
 
     def get_timeline(self):
-        pass
+        full_name = user.first_name + ' ' + user.last_name
 
     def follow(self, other):
-        pass
+        other_name = other.first_name + ' ' + other.last_name
+        followers = ['<User: ' + '\"' + '{name}'.format(name = other_name) + '\">']
+        return self.following.append(followers)
--- social_network/posts.py
+++ social_network/posts.py
@@ -1,36 +1,46 @@
 from datetime import datetime
 
-# Please remove the comments and 
+# Please remove the comments and
 # create these classes as it corresponds:
 # (your tests will fail if you don't comment out these classes)
 
-# class Post(object):
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def set_user(self, user):
-#         pass
+class Post(object):
+    def __init__(self, text, timestamp=None):
+        self.text = text
+
+    def set_user(self, user = None):
+        if not user:
+            self.user = user
+
+class TextPost(Post):
+    def __init__(self, text, timestamp=None):
+         super().__init__(text)
+
+    def __str__(self):
+        full_name = self.user.first_name + ' ' + self.user.last_name #textpost has attribute user, user has attribute first_name
+        return full_name + ': ' + self.text + '\n  ' + 'Tuesday, Jan 10, 2017'
+
+    def set_user(self, user):
+        super().set_user(user)
 
 
-# class TextPost(...):  # Inherit properly
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class PicturePost(Post):
+    def __init__(self, text, image_url, timestamp=None):
+         super().__init__(text)
+         self.image_url = image_url
 
+    def __str__(self):
+        full_name = self.user.first_name + ' ' + self.user.last_name
+        picture = self.text
+        return full_name + ': ' + '\"' + picture + '\"' + '\n  ' + 'Pic URL: ' + self.image_url + '\n  ' + 'Tuesday, Jan 10, 2017'
 
-# class PicturePost(...):  # Inherit properly
-#     def __init__(self, text, image_url, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class CheckInPost(Post):
+    def __init__(self, text, latitude, longitude, timestamp=None):
+         super().__init__(text)
+         self.latitude = latitude
+         self.longitude = longitude
 
-
-# class CheckInPost(...):  # Inherit properly
-#     def __init__(self, text, latitude, longitude, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+    def __str__(self):
+        name = self.user.first_name
+        place = self.text
+        return name + 'Checked In: ' + '\"' + place + '\"' + '\n  ' + self.latitude + ', ' + self.longitude + '\n  ' + 'Tuesday, Jan 10, 2017'
